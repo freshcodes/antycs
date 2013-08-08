8a189aa44fee8abd4ce984419cddc670de4a40d0
Brandon Aaron <brandon.aaron@gmail.com>
2013-08-08 12:08:14 -0500

    youtube plugin placeholder

---
+++ src/plugins/youtube.js
@@ -0,0 +1,5 @@
+// youtube events:
+//  onStateChange: youtube.statechange
+//  onPlaybackQualityChange: youtube.qualitychange
+//  onPlaybackRateChange: youtube.ratechange
+//  onError: youtube.error
